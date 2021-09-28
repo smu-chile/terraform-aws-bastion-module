b066f04866b9b31e5545f55f2b183ac6eaf386cd
cesar-vergara-e <cvergarae@smu.cl>
2021-09-28 10:18:57 -0300

    test

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,13 @@ data "aws_instances" "bastion" {
 output "bastion-public-ips" {
   value = data.aws_instances.bastion.public_ips
 }
+
+data "github_ip_ranges" "test" {}
+
+output "github-public-ips" {
+  value = data.github_ip_ranges.actions_ipv4
+}
+
+output "github-public-ips" {
+  value = data.github_ip_ranges.actions_ipv6
+}
--- provider.tf
+++ provider.tf
@@ -5,5 +5,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.56.0"
     }
+    github = {
+      source = "integrations/github"
+      version = "4.15.1"
+    }
   }
 }
