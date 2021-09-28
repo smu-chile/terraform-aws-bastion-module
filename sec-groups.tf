c89480ff6dd178fc75cfcca943f848790fef1584
cesar-vergara-e <cvergarae@smu.cl>
2021-09-28 11:21:53 -0300

    test vpn

--- sec-groups.tf
+++ sec-groups.tf
@@ -6,7 +6,7 @@ module "ssh_sg" {
   description = "Security group which is to allow SSH from Bastion"
   vpc_id      = var.vpc-id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["186.148.38.0/25,190.98.247.64/26"]
   ingress_rules       = ["ssh-tcp"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
