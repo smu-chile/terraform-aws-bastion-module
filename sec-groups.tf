95181918d104c014c79d83e22a2498da39f7676b
cesar-vergara-e <cvergarae@smu.cl>
2021-09-28 14:59:26 -0300

    test

--- sec-groups.tf
+++ sec-groups.tf
@@ -12,22 +12,20 @@ module "ssh_sg" {
       cidr_blocks = var.ingress-cidr
       description = "SSH from SMU"
     },
-  
+    {
+      rule        = "ssh-tcp"
+      cidr_blocks = join("," , data.github_ip_ranges.test.actions_ipv4)
+      description = "SSH from Github Actions IPV4"
+    },
+        {
+      rule        = "ssh-tcp"
+      cidr_blocks = join("," , data.github_ip_ranges.test.actions_ipv6)
+      description = "SSH from Github Actions IPV6"
+    },
   ]
 
-    // {
-    //   rule        = "ssh-tcp"
-    //   cidr_blocks = join("," , data.github_ip_ranges.test.actions_ipv4)
-    //   description = "SSH from Github Actions IPV4"
-    // },
-    //     {
-    //   rule        = "ssh-tcp"
-    //   cidr_blocks = join("," , data.github_ip_ranges.test.actions_ipv6)
-    //   description = "SSH from Github Actions IPV6"
-    // },
-
-  // egress_cidr_blocks      = ["10.10.0.0/16"]
-  // egress_rules            = ["any"]
+  egress_cidr_blocks      = ["0.0.0.0/0"]
+  egress_rules            = ["any"]
   
   tags = {
     "kubernetes.io/cluster/${var.name}" = "owned"
