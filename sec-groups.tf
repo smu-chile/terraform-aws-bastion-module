25788495fa7afa8a99c567622b59115b0b49fe54
cesar-vergara-e <cvergarae@smu.cl>
2021-09-29 09:35:29 -0300

    test

--- sec-groups.tf
+++ sec-groups.tf
@@ -1,14 +1,5 @@
-module "ssh_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.3.0"
-
-  name        = "ssh-sg"
-  description = "Security group which is to allow SSH from Bastion"
-  vpc_id      = var.vpc-id
-
-
-  locals {
-    ingress_rules = var.var.ingress-cidr-ansible != "" ? 
+locals {
+    ingress_rules = var.ingress-cidr-ansible != "" ? [
     {
       rule        = "ssh-tcp"
       cidr_blocks = var.ingress-cidr
@@ -18,20 +9,27 @@ module "ssh_sg" {
       rule        = "ssh-tcp"
       cidr_blocks = var.ingress-cidr-ansible
       description = "SSH from Allowed cidr"
-    } :
+    },
+    ] : [
     {
       rule        = "ssh-tcp"
       cidr_blocks = var.ingress-cidr
       description = "SSH from Allowed cidr"
     }
-  }
+    ]
 
+}
 
-  ingress_with_cidr_blocks = [
-    local.ingress_rules
-  ]
+module "ssh_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.3.0"
 
+  name        = "ssh-sg"
+  description = "Security group which is to allow SSH from Bastion"
+  vpc_id      = var.vpc-id
 
+  ingress_with_cidr_blocks = locals.ingress_rules
+  
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
 
